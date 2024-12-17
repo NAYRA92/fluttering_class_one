a3389877595d897f84b12ceca6b892c423eb090c
naira.hashem@gmail.com <naira.hashem@gmail.com>
2024-12-17 09:55:21 +0300

    modified:   lib/login_page.dart 	modified:   lib/main.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttering_class_one/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -17,79 +18,97 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Fluttering Course"),
-      ),
-
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 20.0,),
-            Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25),
-                  bottomRight: Radius.circular(25),
-                ),
-                image: DecorationImage(
-                  image: NetworkImage("https://flutter-ko.dev/assets/images/docs/catalog-widget-placeholder.png"),
-                  fit: BoxFit.cover,
-                  opacity: 0.3
-                  ),
-                  border: Border.all(),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 1.5,
-                      spreadRadius: 1
-                    )
-                  ]
-              ),
-            ),
-
-            TextFormField(
-              controller: _username,
-            ),
-
-
-            TextFormField(
-              controller: _password,
-            ),
-
-            ElevatedButton(
-              onPressed: (){
-                // setState(() {
-                //   textname =  _username.text; //setState to change the text حالياً
-                // });
-
-                if(_username.text == "nayra"
-                && _password.text == "123"){
-                  setState(() {
-                  textname =  "تم تسجيل الدخول بنجاح";
-                });
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(
-                    builder: (context)=> HomePage(
-                    user_name: _username.text,
-                    )),
-                );
-                }
-                else {
-                   setState(() {
-                  textname =  "اعد المحاولة مرة أخرى"; //setState to change the text حالياً
-                });
-                }
-              }, 
-              child: Text("تسجيل الدخول")),
-
-            Text(textname)
-          ],
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Icon(Icons.flutter_dash_rounded),
+              Text("كورس فلاترينج",
+              style: GoogleFonts.tajawal(
+                color: Colors.blue
+              ) ,),
+            ],
           ),
+        ),
+      
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20.0,),
+              Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                  image: DecorationImage(
+                    image: NetworkImage("https://flutter-ko.dev/assets/images/docs/catalog-widget-placeholder.png"),
+                    fit: BoxFit.cover,
+                    opacity: 0.3
+                    ),
+                    border: Border.all(),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 1.5,
+                        spreadRadius: 1
+                      )
+                    ]
+                ),
+              ),
+      
+              TextFormField(
+                controller: _username,
+                decoration: InputDecoration(
+                  hintText: "أكتب اسم المستخدم"
+                ),
+              ),
+      
+      
+              TextFormField(
+                controller: _password,
+                decoration: InputDecoration(
+                  hintText: "أكتب كلمة المرور"
+                ),
+                obscureText: true,
+              ),
+      
+              ElevatedButton(
+                onPressed: (){
+                  // setState(() {
+                  //   textname =  _username.text; //setState to change the text حالياً
+                  // });
+      
+                  if(_username.text == "nayra"
+                  && _password.text == "123"){
+                    setState(() {
+                    textname =  "تم تسجيل الدخول بنجاح";
+                  });
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (context)=> HomePage(
+                      user_name: _username.text,
+                      )),
+                  );
+                  }
+                  else {
+                     setState(() {
+                    textname =  "اعد المحاولة مرة أخرى"; //setState to change the text حالياً
+                  });
+                  }
+                }, 
+                child: Text("تسجيل الدخول")),
+      
+              Text(textname)
+            ],
+            ),
+        ),
       ),
     );
   }
